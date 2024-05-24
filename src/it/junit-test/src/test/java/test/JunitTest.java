030682fddc188741ababca01819058956a4c9f76
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-24 16:40:32 +1000

    gwt build fixes
    - Removed bad imports
    - Fixed a module name reference

--- src/it/junit-test/src/test/java/test/JunitTest.java
+++ src/it/junit-test/src/test/java/test/JunitTest.java
@@ -19,17 +19,8 @@ package test;
 import com.google.j2cl.junit.apt.J2clTestInput;
 import org.junit.Assert;
 import org.junit.Test;
-
-// copied parse Sample
 @J2clTestInput(JunitTest.class)
 public class JunitTest {
-
-    private final static ExpressionNumberKind EXPRESSION_NUMBER_KIND = ExpressionNumberKind.DEFAULT;
-
-    private static final Supplier<LocalDateTime> NOW = LocalDateTime::now;
-
-    private final static SpreadsheetLabelNameResolver LABEL_NAME_RESOLVER = SpreadsheetLabelNameResolvers.fake();
-
     @Test
     public void testMetadataNonLocaleDefaults() {
         Assert.assertEquals(1, 3 - 2);
