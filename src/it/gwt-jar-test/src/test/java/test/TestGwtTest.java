f33e400b7b7512bf7ebc3fc9bab58b22e7e70283
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-25 22:51:19 +1000

    TestGwtTest & JUnitTest PluginName tests

--- src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
+++ src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
@@ -1,6 +1,12 @@
 package test;
 
 import com.google.gwt.junit.client.GWTTestCase;
+
+import java.util.Comparator;
+import java.util.List;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.plugin.PluginName;
+
 public class TestGwtTest extends GWTTestCase {
 
     @Override
@@ -12,6 +18,25 @@ public class TestGwtTest extends GWTTestCase {
         assertEquals(
                 1,
                 1
+        );
+    }
+
+    public void testPluginNameSort() {
+        final PluginName a = PluginName.with("STRING");
+        final PluginName b = PluginName.with("date-of-month");
+        final PluginName c = PluginName.with("text-case-insensitive");
+        final PluginName d = PluginName.with("month-of-year");
+
+        final List<PluginName> list = Lists.array();
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        list.add(d);
+        list.sort(Comparator.naturalOrder());
+
+        assertEquals(
+                Lists.of(a, b, d, c),
+                list
         );
     }
 }
--- src/it/junit-test/src/test/java/test/JunitTest.java
+++ src/it/junit-test/src/test/java/test/JunitTest.java
@@ -19,11 +19,37 @@ package test;
 import com.google.j2cl.junit.apt.J2clTestInput;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.List;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.plugin.PluginName;
+
 @J2clTestInput(JunitTest.class)
 public class JunitTest {
     @Test
     public void testMetadataNonLocaleDefaults() {
         Assert.assertEquals(1, 3 - 2);
+    }
+
+    @Test
+    public void testPluginNameSort() {
+        final PluginName a = PluginName.with("STRING");
+        final PluginName b = PluginName.with("date-of-month");
+        final PluginName c = PluginName.with("text-case-insensitive");
+        final PluginName d = PluginName.with("month-of-year");
+
+        final List<PluginName> list = Lists.array();
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        list.add(d);
+        list.sort(Comparator.naturalOrder());
+
+        Assert.assertEquals(
+                Lists.of(a, b, d, c),
+                list
+        );
     }
 }
 
