020f6ec02b0331d84ffb1331716d2bdc0d7f65b3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-07-09 12:10:04 +1000

    LabelledText

---
+++ src/test/java/walkingkooka/plugin/LabelledTextTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.plugin;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.HashCodeEqualsDefinedTesting2;
+import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.text.HasTextTesting;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class LabelledTextTest implements HashCodeEqualsDefinedTesting2<LabelledText>,
+        HasTextTesting,
+        ClassTesting2<LabelledText> {
+
+    private final static String LABEL = "Label123";
+
+    private final static String TEXT = "Text123";
+
+    @Test
+    public void testWithNullLabelFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> LabelledText.with(
+                        null,
+                        TEXT
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullLTextFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> LabelledText.with(
+                        LABEL,
+                        null
+                )
+        );
+    }
+
+    @Test
+    public void testWith() {
+        final LabelledText labelledText = LabelledText.with(
+                LABEL,
+                TEXT
+        );
+        this.checkEquals(
+                LABEL,
+                labelledText.label(),
+                "label"
+        );
+        this.textAndCheck(
+                labelledText,
+                TEXT
+        );
+    }
+
+    @Test
+    public void testWithEmptyLabelAndEmptyText() {
+        final String label = "";
+        final String text = "";
+
+        final LabelledText labelledText = LabelledText.with(
+                label,
+                text
+        );
+        this.checkEquals(
+                label,
+                labelledText.label(),
+                "label"
+        );
+        this.textAndCheck(
+                labelledText,
+                text
+        );
+    }
+
+    // hashCode/equals..................................................................................................
+
+    @Test
+    public void testEqualsDifferentLabel() {
+        this.checkNotEquals(
+                LabelledText.with(
+                        "different " + LABEL,
+                        TEXT
+                )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentText() {
+        this.checkNotEquals(
+                LabelledText.with(
+                        LABEL,
+                        "different " + TEXT
+                )
+        );
+    }
+
+    @Override
+    public LabelledText createObject() {
+        return LabelledText.with(
+                LABEL,
+                TEXT
+        );
+    }
+
+    // Class............................................................................................................
+
+    @Override
+    public Class<LabelledText> type() {
+        return LabelledText.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
