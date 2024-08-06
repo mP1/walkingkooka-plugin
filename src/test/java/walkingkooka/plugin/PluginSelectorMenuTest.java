d6ab13a066d8e86c6b49ff7a26b2731ef32a18f1
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-06 18:33:31 +1000

    PluginSelectorMenu

---
+++ src/test/java/walkingkooka/plugin/PluginSelectorMenuTest.java
@@ -0,0 +1,273 @@
+/*
+ * Copyright 2024 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.plugin;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.HashCodeEqualsDefinedTesting2;
+import walkingkooka.naming.Names;
+import walkingkooka.naming.StringName;
+import walkingkooka.plugin.PluginSelectorMenuTest.TestPluginSelector;
+import walkingkooka.reflect.ClassTesting;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.text.printer.TreePrintableTesting;
+import walkingkooka.tree.expression.ExpressionNumberKind;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeContext;
+import walkingkooka.tree.json.marshall.JsonNodeMarshallContext;
+import walkingkooka.tree.json.marshall.JsonNodeMarshallContexts;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContexts;
+
+import java.math.MathContext;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class PluginSelectorMenuTest implements ClassTesting<PluginSelectorMenu<TestPluginSelector, StringName>>,
+        HashCodeEqualsDefinedTesting2<PluginSelectorMenu<TestPluginSelector, StringName>>,
+        TreePrintableTesting {
+
+    private final static String LABEL = "Label Short123";
+
+    private final static TestPluginSelector SELECTOR = new TestPluginSelector(
+            PluginSelector.parse(
+                    "TestSelector",
+                    Names::string
+            )
+    );
+
+    // with.............................................................................................................
+
+    @Test
+    public void testWithNullLabelFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> PluginSelectorMenu.with(
+                        null,
+                        SELECTOR
+                )
+        );
+    }
+
+    @Test
+    public void testWithEmptyLabelFails() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> PluginSelectorMenu.with(
+                        "",
+                        SELECTOR
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullSelectorFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> PluginSelectorMenu.with(
+                        LABEL,
+                        null
+                )
+        );
+    }
+
+    @Test
+    public void testWith() {
+        final PluginSelectorMenu<TestPluginSelector, StringName> menuItem = PluginSelectorMenu.with(
+                LABEL,
+                SELECTOR
+        );
+        this.checkEquals(
+                LABEL,
+                menuItem.label(),
+                "label"
+        );
+        this.checkEquals(
+                SELECTOR,
+                menuItem.selector(),
+                "selector"
+        );
+    }
+
+    // hashCode/equals..................................................................................................
+
+    @Test
+    public void testEqualsDifferentLabel() {
+        this.checkNotEquals(
+                PluginSelectorMenu.with(
+                        "different " + LABEL,
+                        SELECTOR
+                )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentSelector() {
+        this.checkNotEquals(
+                PluginSelectorMenu.with(
+                        LABEL,
+                        new TestPluginSelector(
+                                PluginSelector.parse(
+                                        "TestSelectorDifferent",
+                                        Names::string
+                                )
+                        )
+                )
+        );
+    }
+
+    @Override
+    public PluginSelectorMenu<TestPluginSelector, StringName> createObject() {
+        return PluginSelectorMenu.with(
+                LABEL,
+                SELECTOR
+        );
+    }
+
+    // json.............................................................................................................
+
+    private final static JsonNode JSON = JsonNode.parse("{\n" +
+            "  \"label\": \"Label Short123\",\n" +
+            "  \"selector\": \"TestSelector\"\n" +
+            "}");
+
+    @Test
+    public void testMarshall() {
+        this.checkEquals(
+                JSON,
+                PluginSelectorMenu.with(
+                        LABEL,
+                        SELECTOR
+                ).marshall(JsonNodeMarshallContexts.basic())
+        );
+    }
+
+    @Test
+    public void testUnmarshall() {
+        this.checkEquals(
+                PluginSelectorMenu.with(
+                        LABEL,
+                        SELECTOR
+                ),
+                PluginSelectorMenu.unmarshall(
+                        JSON,
+                        JsonNodeUnmarshallContexts.basic(
+                                ExpressionNumberKind.BIG_DECIMAL,
+                                MathContext.DECIMAL32
+                        ),
+                        TestPluginSelector.class
+                )
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<PluginSelectorMenu<TestPluginSelector, StringName>> type() {
+        return Cast.to(
+                PluginSelectorMenu.class
+        );
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+
+    static class TestPluginSelector implements PluginSelectorLike<StringName> {
+
+        static TestPluginSelector parse(final String selector) {
+            return new TestPluginSelector(
+                    PluginSelector.parse(
+                            selector,
+                            Names::string
+                    )
+            );
+        }
+
+        TestPluginSelector(final PluginSelector<StringName> pluginSelector) {
+            this.pluginSelector = pluginSelector;
+        }
+
+        @Override
+        public PluginSelectorLike<StringName> setName(final StringName name) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public PluginSelectorLike<StringName> setText(final String text) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public PluginSelectorLike<StringName> setValues(final List<?> values) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public StringName name() {
+            return this.pluginSelector.name();
+        }
+
+        @Override
+        public String text() {
+            return this.pluginSelector.text();
+        }
+
+        @Override
+        public void printTree(final IndentingPrinter printer) {
+            this.pluginSelector.printTree(printer);
+        }
+
+        private final PluginSelector<StringName> pluginSelector;
+
+        @Override
+        public int hashCode() {
+            return this.pluginSelector.hashCode();
+        }
+
+        @Override
+        public boolean equals(final Object other) {
+            return this == other || other instanceof TestPluginSelector && this.equals0((TestPluginSelector)other);
+        }
+
+        private boolean equals0(final TestPluginSelector other) {
+            return this.pluginSelector.equals(other.pluginSelector);
+        }
+
+        JsonNode marshall(final JsonNodeMarshallContext context) {
+            return this.pluginSelector.marshall(context);
+        }
+
+        static TestPluginSelector unmarshall(final JsonNode json,
+                                             final JsonNodeUnmarshallContext context) {
+            return parse(json.stringOrFail());
+        }
+    }
+
+    static {
+        JsonNodeContext.register(
+                JsonNodeContext.computeTypeName(TestPluginSelector.class),
+                TestPluginSelector::unmarshall,
+                TestPluginSelector::marshall,
+                TestPluginSelector.class
+        );
+    }
+}
