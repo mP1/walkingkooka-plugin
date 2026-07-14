2bcd102c4d24336de3dea2826d0fab57061f59b5
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-14 22:13:35 +1000

    https://github.com/mP1/walkingkooka/pull/3195 HasCharsetTesting.CHARSET constant
    - https://github.com/mP1/walkingkooka/pull/3195
    - HasCharsetTesting.CHARSET constant

--- src/test/java/walkingkooka/plugin/ReadOnlyProviderContextTest.java
+++ src/test/java/walkingkooka/plugin/ReadOnlyProviderContextTest.java
@@ -47,8 +47,6 @@ public final class ReadOnlyProviderContextTest implements ProviderContextTesting
 
     private final static ConverterLike CAN_CONVERT = ConverterContexts.fake();
 
-    private final static Charset CHARSET = StandardCharsets.UTF_8;
-    
     private final static Currency CURRENCY = Currency.getInstance("AUD");
 
     private final static LineEnding LINE_ENDING = LineEnding.NL;
