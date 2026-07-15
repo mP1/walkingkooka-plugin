612dc36aa1ccc2ef7b6d3a3197a2760f3df2ccef
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-15 11:58:07 +1000

    https://github.com/mP1/walkingkooka/pull/3197 HasLineEndingTesting.LINE_ENDING constant
    - https://github.com/mP1/walkingkooka/pull/3197
    - HasLineEndingTesting.LINE_ENDING constant

--- src/test/java/walkingkooka/plugin/ReadOnlyProviderContextTest.java
+++ src/test/java/walkingkooka/plugin/ReadOnlyProviderContextTest.java
@@ -47,8 +47,6 @@ public final class ReadOnlyProviderContextTest implements ProviderContextTesting
 
     private final static ConverterLike CAN_CONVERT = ConverterContexts.fake();
 
-    private final static LineEnding LINE_ENDING = LineEnding.NL;
-
     private final static Locale LOCALE = Locale.forLanguageTag("en-AU");
 
     private final static LocalDateTime NOW = LocalDateTime.of(
