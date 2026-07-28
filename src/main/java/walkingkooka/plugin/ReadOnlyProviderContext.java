9c343ae50ea589d73374d032fc3c26ff7ca7683a
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-28 11:43:37 +1000

    ReadOnlyProviderContext implements EnvironmentContextDelegator

--- src/main/java/walkingkooka/plugin/ReadOnlyProviderContext.java
+++ src/main/java/walkingkooka/plugin/ReadOnlyProviderContext.java
@@ -19,27 +19,19 @@ package walkingkooka.plugin;
 
 import walkingkooka.Either;
 import walkingkooka.environment.EnvironmentContext;
-import walkingkooka.environment.EnvironmentValueName;
-import walkingkooka.environment.EnvironmentWatcher;
-import walkingkooka.net.email.EmailAddress;
+import walkingkooka.environment.EnvironmentContextDelegator;
+import walkingkooka.environment.EnvironmentContexts;
 import walkingkooka.plugin.store.PluginStore;
-import walkingkooka.text.Indentation;
-import walkingkooka.text.LineEnding;
+import walkingkooka.predicate.Predicates;
 
-import java.nio.charset.Charset;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Currency;
-import java.util.Locale;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
 
 /**
  * A {@link ProviderContext} where all setXXX and removeXXX methods throw {@link UnsupportedOperationException}.
  * Note the {@link #cloneEnvironment()} returns a clone of the wrapped {@link ProviderContext}.
  */
-final class ReadOnlyProviderContext implements ProviderContext {
+final class ReadOnlyProviderContext implements ProviderContext,
+    EnvironmentContextDelegator {
 
     static ReadOnlyProviderContext with(final ProviderContext context) {
         ReadOnlyProviderContext readOnlyProviderContext;
@@ -57,7 +49,19 @@ final class ReadOnlyProviderContext implements ProviderContext {
 
     private ReadOnlyProviderContext(final ProviderContext context) {
         this.context = context;
+
+        this.readOnlyEnvironmentContext = EnvironmentContexts.readOnly(
+            Predicates.always(), // all values are readonly
+            context
+        );
     }
+
+    @Override
+    public PluginStore pluginStore() {
+        return this.context.pluginStore();
+    }
+
+    // ConverterLike....................................................................................................
 
     @Override
     public boolean canConvert(final Object value,
@@ -77,6 +81,8 @@ final class ReadOnlyProviderContext implements ProviderContext {
         );
     }
 
+    // EnvironmentContext...............................................................................................
+
     @Override
     public ProviderContext cloneEnvironment() {
         return this.setEnvironmentContext(
@@ -93,137 +99,14 @@ final class ReadOnlyProviderContext implements ProviderContext {
             after;
     }
 
-    @Override
-    public <T> Optional<T> environmentValue(final EnvironmentValueName<T> name) {
-        return this.context.environmentValue(name);
-    }
+    // EnvironmentContextDelegator......................................................................................
 
     @Override
-    public Set<EnvironmentValueName<?>> environmentValueNames() {
-        return this.context.environmentValueNames();
+    public EnvironmentContext environmentContext() {
+        return this.readOnlyEnvironmentContext;
     }
 
-    @Override
-    public <T> void setEnvironmentValue(final EnvironmentValueName<T> name,
-                                        final T value) {
-        Objects.requireNonNull(name, "name");
-        Objects.requireNonNull(value, "value");
-
-        throw name.readOnlyEnvironmentValueException();
-    }
-
-    @Override
-    public void removeEnvironmentValue(final EnvironmentValueName<?> name) {
-        Objects.requireNonNull(name, "name");
-
-        throw name.readOnlyEnvironmentValueException();
-    }
-
-    @Override
-    public Optional<EmailAddress> user() {
-        return this.context.user();
-    }
-
-    @Override
-    public void setUser(final Optional<EmailAddress> user) {
-        Objects.requireNonNull(user, "user");
-
-        throw USER.readOnlyEnvironmentValueException();
-    }
-
-    @Override
-    public LocalDateTime now() {
-        return this.context.now();
-    }
-
-    @Override
-    public Charset charset() {
-        return this.context.charset();
-    }
-
-    @Override
-    public void setCharset(final Charset charset) {
-        Objects.requireNonNull(charset, "charset");
-
-        throw CHARSET.readOnlyEnvironmentValueException();
-    }
-    
-    @Override
-    public Currency currency() {
-        return this.context.currency();
-    }
-
-    @Override
-    public void setCurrency(final Currency currency) {
-        Objects.requireNonNull(currency, "currency");
-
-        throw CURRENCY.readOnlyEnvironmentValueException();
-    }
-    
-    @Override
-    public Indentation indentation() {
-        return this.context.indentation();
-    }
-
-    @Override
-    public void setIndentation(final Indentation indentation) {
-        Objects.requireNonNull(indentation, "indentation");
-
-        throw INDENTATION.readOnlyEnvironmentValueException();
-    }
-    
-    @Override
-    public LineEnding lineEnding() {
-        return this.context.lineEnding();
-    }
-
-    @Override
-    public void setLineEnding(final LineEnding lineEnding) {
-        Objects.requireNonNull(lineEnding, "lineEnding");
-
-        throw LINE_ENDING.readOnlyEnvironmentValueException();
-    }
-    
-    @Override
-    public Locale locale() {
-        return this.context.locale();
-    }
-
-    @Override
-    public void setLocale(final Locale locale) {
-        Objects.requireNonNull(locale, "locale");
-
-        throw LOCALE.readOnlyEnvironmentValueException();
-    }
-
-    @Override
-    public ZoneOffset timeOffset() {
-        return this.context.timeOffset();
-    }
-
-    @Override
-    public void setTimeOffset(final ZoneOffset timeOffset) {
-        Objects.requireNonNull(timeOffset, "timeOffset");
-
-        throw TIME_OFFSET.readOnlyEnvironmentValueException();
-    }
-
-    @Override
-    public PluginStore pluginStore() {
-        return this.context.pluginStore();
-    }
-
-    @Override
-    public Runnable addEnvironmentWatcher(final EnvironmentWatcher watcher) {
-        Objects.requireNonNull(watcher, "watcher");
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Runnable addEnvironmentWatcherOnce(final EnvironmentWatcher watcher) {
-        Objects.requireNonNull(watcher, "watcher");
-        throw new UnsupportedOperationException();
-    }
+    private final EnvironmentContext readOnlyEnvironmentContext;
 
     private final ProviderContext context;
 
