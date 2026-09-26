7dc3b03f2eabff642e592facea65fb4af023e28d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-26 12:50:23 +1000

    https://github.com/mP1/walkingkooka-environment/pull/576 EnvironmentContextSharedMap fire value change debug message
    - https://github.com/mP1/walkingkooka-environment/pull/576
    - EnvironmentContextSharedMap fire value change debug message

--- src/test/java/walkingkooka/plugin/ProviderContextBasicTest.java
+++ src/test/java/walkingkooka/plugin/ProviderContextBasicTest.java
@@ -344,7 +344,8 @@ public final class ProviderContextBasicTest implements ProviderContextTesting<Pr
         context.debug(MESSAGE1);
 
         this.checkEquals(
-            "DEBUG " + MESSAGE1 + LINE_ENDING,
+            "DEBUG fire value change loggingLevel=NONE to loggingLevel=DEBUG\n" +
+                "DEBUG Message111\n",
             b.toString()
         );
     }
